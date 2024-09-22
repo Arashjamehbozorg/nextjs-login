4103b91138ec706f6d836add176e509811484031
Arash Jamehbozorg <arashjamehbozorg@MacBook-Air-von-Arash.local>
2024-09-22 14:55:21 +0330

    solved the bug with avatar icon in member-avatar.tsx, the responsiveness of icons image is certain

--- src/app/profile/member/member-avatar.tsx
+++ src/app/profile/member/member-avatar.tsx
@@ -3,12 +3,7 @@ import avatarIcon from "../../../../public/avatar.png";
 export default function MemberAvatar() {
   return (
     <div className="rounded-full h-14 w-14 relative border-2 border-[#79B4E4] flex justify-center items-center overflow-hidden">
-      <Image
-        alt="avatar"
-        src={avatarIcon}
-        fill
-        style={{ objectFit: "contain" }}
-      ></Image>
+      <Image alt="avatar" src={avatarIcon} layout="responsive"></Image>
     </div>
   );
 }
